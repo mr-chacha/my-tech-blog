4c1b6cfb98814c1fe9f20bb955f09caaf0bcf9e1
차차 <117566375+mr-chacha@users.noreply.github.com>
2025-08-01 19:52:25 +0900

    feat : 파이어 베이스 로그인 구현(깃허브, 이메일)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import {GlobalStyles} from "@/common/style";
 import {Footer, Header} from "@/components/layout";
-import {AboutPage, DetailPage, FormPage, MainPage} from "@/pages";
+import {AboutPage, DetailPage, FormPage, LoginPage, MainPage} from "@/pages";
 import React, {useEffect} from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import styled from "styled-components";
@@ -9,20 +9,31 @@ function App() {
   const {isDarkMode} = useZustandStore();
   const pathname = window.location.pathname;
   const layout = pathname.includes("/form");
+  const isLoginPage = pathname === "/login";
 
   return (
     <BrowserRouter>
       <GlobalStyles isDarkMode={isDarkMode} />
-      <Header />
-      <Layout layout={layout}>
+      {isLoginPage ? (
+        // 로그인 페이지일 때
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/form" element={<FormPage />} />
-          <Route path="/post/:id" element={<DetailPage />} />
+          <Route path="/login" element={<LoginPage />} />
         </Routes>
-      </Layout>
-      <Footer />
+      ) : (
+        // 다른 페이지들일 때
+        <>
+          <Header />
+          <Layout $layout={layout}>
+            <Routes>
+              <Route path="/" element={<MainPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="/form" element={<FormPage />} />
+              <Route path="/post/:id" element={<DetailPage />} />
+            </Routes>
+          </Layout>
+          <Footer />
+        </>
+      )}
     </BrowserRouter>
   );
 }
@@ -31,7 +42,7 @@ export default App;
 
 const Layout = styled.main`
   margin: 60px auto 0px;
-  max-width: ${(props) => (props?.layout ? "100%" : "1200px")};
+  max-width: ${(props) => (props?.$layout ? "100%" : "1200px")};
   margin-top: 64px;
   background-color: var(--Back-Color);
 `;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export {MainPage} from "./MainPage";
 export {AboutPage} from "./AboutPage";
 export {FormPage} from "./FormPage";
 export {DetailPage} from "./DetailPage";
+export {LoginPage} from "./LoginPage";
