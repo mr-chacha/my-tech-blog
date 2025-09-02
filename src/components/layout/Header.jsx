13268f6bd531c9271da00374945956ffd431ccf4
차차 <117566375+mr-chacha@users.noreply.github.com>
2025-09-02 09:43:35 +0900

    fix : 헤더 로그인 페이지 삭제

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -93,17 +93,6 @@ export const Header = () => {
           >
             About
           </NavTitle>
-          <NavTitle
-            className="header-title"
-            font="var(--Body-M)"
-            $isActive={headerMenu === "login"}
-            onClick={(e) => {
-              e.preventDefault();
-              navHandler("/login", "login");
-            }}
-          >
-            Login
-          </NavTitle>
           {userInfo && (
             <NavTitle
               className="header-title"
