d4a3e09e0ef8c836d079beccdbce42526fda52e2
차차 <117566375+mr-chacha@users.noreply.github.com>
2026-04-05 12:40:53 +0900

    fix : env

--- next.config.js
+++ next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  env: {
+    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+    FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
+    ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+    S3_ACCESS_KEY_ID: process.env.S3_ACCESS_KEY_ID,
+    S3_SECRET_ACCESS_KEY: process.env.S3_SECRET_ACCESS_KEY,
+    S3_REGION: process.env.S3_REGION,
+    S3_BUCKET_NAME: process.env.S3_BUCKET_NAME,
+  },
   images: {
     remotePatterns: [
       {
